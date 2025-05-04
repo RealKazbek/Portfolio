835e9b105edb56049d659d4107a70d64f6ca5dbb
ka33bek <37765@iitu.edu.kz>
2025-05-04 21:58:06 +0500

    Changes: Stylization of the quote section

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -6,17 +6,17 @@ function Quote() {
       <span className="absolute inline-block w-23 h-23 border border-[var(--gray)] mt-7 mr-[-80px] lg:mr-[-30px]"></span>
       <blockquote
         cite="https://chatgpt.com/"
-        className="relative px-4 md:px-0 md:max-w-[712px] mx-auto flex flex-col items-end text-xl"
+        className="relative px-4 md:px-0 md:max-w-[712px] mx-auto flex flex-col items-end text-lg md:text-xl"
       >
         <p
-          className="border p-8 border-[var(--gray)] font-medium
-        before:content-['❛❛'] before:absolute before:-top-5 before:font-medium before:text-6xl before:text-[var(--gray)] before:bg-[var(--background)] before:bg-size-[100%_40%] before:bg-no-repeat
-        after:content-['❟❟'] after:absolute after:bottom-12 after:right-8 md:after:right-4 after:font-medium after:text-6xl after:text-[var(--gray)] after:bg-[var(--background)] after:bg-size-[100%_40%] after:bg-no-repeat"
+          className={`border p-4 md:p-8 border-[var(--gray)] font-medium
+        before:content-['\"'] before:h-7 before:absolute before:-top-4 before:font-medium before:text-6xl before:text-[var(--gray)] before:bg-[var(--background)]
+        after:content-['\"'] after:h-7 after:absolute after:bottom-8 md:after:bottom-12 after:rotate-180 after:right-8 md:after:right-4 after:font-medium after:text-6xl after:text-[var(--gray)] after:bg-[var(--background)]`}
         >
           Any fool can write code that a computer can understand. Good
           programmers write code that humans can understand.
         </p>
-        <footer className="border p-4 border-[var(--gray)]">
+        <footer className="border p-2 md:p-4 border-[var(--gray)]">
           — Martin Fowler
         </footer>
       </blockquote>

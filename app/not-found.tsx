71ecfd6c3011444ea6339fc852aaa4567f5abcae
ka33bek <37765@iitu.edu.kz>
2025-05-04 13:06:07 +0500

    feat: improved UI for 404 notFound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className='flex flex-col items-center justify-center animate-pulse'>
+    <div className='mt-10 flex flex-col items-center justify-center animate-pulse gap-1'>
+      <span className='text-9xl text-[var(--primary))]'>404</span>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/" className='text-[var(--gray)] transition-colors duration-300 ease-in-out hover:text-[var(--white)]'>Return Home</Link>

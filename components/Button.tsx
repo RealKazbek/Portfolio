96acd3b6e575bf8f77ba9a41f0d290bb98579cb1
ka33bek <37765@iitu.edu.kz>
2025-05-04 19:14:04 +0500

    Fix: Update Button component to use <a> tag inside <Link> for proper link behavior

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
   children: React.ReactNode;
   className?: string;
 }
 
-function Button({ href, children, className = "", ...props }: ButtonProps) {
+function Button({ href = "/", children, className = "", ...props }: ButtonProps) {
   const baseClass =
     "border border-[var(--primary)] py-2 px-4 font-medium transition-colors duration-300 ease-in-out hover:bg-[var(--opacity)]";
 
   return (
-    <a href={href} className={`${baseClass} ${className}`} {...props}>
-      {children}
-    </a>
+    <Link href={href}>
+      <span className={`${baseClass} ${className}`} {...props}>
+        {children}
+      </span>
+    </Link>
   );
 }
 

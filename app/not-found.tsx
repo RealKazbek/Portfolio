987fcf6f0b8069a5fea5bc890c475ae3a2494569
ka33bek <37765@iitu.edu.kz>
2025-05-04 19:37:05 +0500

    Changed: margin at 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className='mt-10 flex flex-col items-center justify-center animate-pulse gap-1'>
+    <div className='mt-30 flex flex-col items-center justify-center animate-pulse gap-1'>
       <span className='text-9xl text-[var(--primary))]'>404</span>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>

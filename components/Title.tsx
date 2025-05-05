5ec66cac5c0cb85277943c0167fffe77e6c979b0
ka33bek <37765@iitu.edu.kz>
2025-05-05 12:38:09 +0500

    Added the title component

---
+++ components/Title.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+
+interface TitleProps {
+  href?: string;
+  children: string;
+}
+
+function Title({ href, children }: TitleProps) {
+  return (
+    <div className="px-4 xl:px-0 xl:max-w-[1024px] mx-auto py-4 flex justify-between items-center font-medium">
+      <div className="flex items-center text-3xl lowercase flex-1">
+        <span className="text-[var(--primary)] mr-2">#</span>
+        <span>{children}</span>
+        <span className="ml-4 mr-[20%] flex-grow h-0.5 bg-[var(--primary)]"></span>
+      </div>
+
+      {href && (
+        <Link
+          href={`/${href}`}
+          className="text-right text-sm hover:text-[var(--primary)] hover:animate-pulse transition duration-300 ease-in-out ml-4"
+        >
+          View all &rarr;
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default Title;
